662a3ea3e61ec16db613b822cdb1ba26e63e3447
André Luiz da Silva <aannddrree.silva@gmail.com>
2024-05-02 15:52:33 -0300

    Update Criarmassa.scala

--- Criarmassa.scala
+++ Criarmassa.scala
@@ -1,3 +1,50 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object AddressDataGenerator {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("AddressDataGenerator")
+      .master("local[*]")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    // Função para gerar um número aleatório dentro de um intervalo
+    def randomInRange(min: Int, max: Int) = {
+      min + scala.util.Random.nextInt((max - min) + 1)
+    }
+
+    // Função para gerar um endereço aleatório
+    def generateAddress(): (String, String, String, String, String) = {
+      val streets = Array("Rua A", "Rua B", "Rua C", "Avenida X", "Avenida Y")
+      val cities = Array("São Paulo", "Rio de Janeiro", "Belo Horizonte", "Brasília", "Curitiba")
+      val states = Array("SP", "RJ", "MG", "DF", "PR")
+      val zip = randomInRange(10000, 99999).toString
+      val street = streets(randomInRange(0, streets.length - 1))
+      val city = cities(randomInRange(0, cities.length - 1))
+      val state = states(randomInRange(0, states.length - 1))
+      val number = randomInRange(1, 100).toString
+      (street, number, city, state, zip)
+    }
+
+    // Gerar 1000 endereços aleatórios
+    val addressData = (1 to 1000).map(_ => generateAddress()).toSeq.toDS()
+
+    // Criar DataFrame a partir dos dados gerados
+    val df = addressData.toDF("street", "number", "city", "state", "zip")
+
+    // Salvar os dados como um arquivo CSV
+    df.write.mode("overwrite").csv("/caminho/para/salvar/os/dados")
+
+    spark.stop()
+  }
+}
+
+
+
+
+
 import org.apache.spark.sql.{SparkSession, Row}
 import org.apache.spark.sql.types._
 
